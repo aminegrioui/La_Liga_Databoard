12ca2a11abf0d524e147ca96566f4e2f120e6222
aminegrioui <amine23grioui@gmail.com>
2021-06-08 22:35:21 +0200

    Add test getWinnersOfSeason API

--- src/test/java/com/springbootproject/laliga/LaLigaProject/LaLigaProjectApplicationTests.java
+++ src/test/java/com/springbootproject/laliga/LaLigaProject/LaLigaProjectApplicationTests.java
@@ -1,13 +1,21 @@
 package com.springbootproject.laliga.LaLigaProject;
 
+import com.springbootproject.laliga.LaLigaProject.service.TeamWinnerService;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
 
 @SpringBootTest
 class LaLigaProjectApplicationTests {
+   @Autowired
+	TeamWinnerService teamWinnerService;
 
-	@Test
-	void contextLoads() {
+   @Test
+    public  void test() throws IOException {
+	   Assert.assertEquals("2019-20",teamWinnerService.getTeamWinner("2019-20").getSeason());
 	}
 
 }
